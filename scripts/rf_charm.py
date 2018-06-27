fb02c0201a88bbd56e8ac1cea2b38cf4ffb89052
miki208 <miloss208@gmail.com>
2018-06-27 16:00:16 +0200

    Pass random_state parameter to the classifier.

--- scripts/rf_charm.py
+++ scripts/rf_charm.py
@@ -25,27 +25,27 @@ scaler.fit(X_train)
 X_train = scaler.transform(X_train)
 X_test = scaler.transform(X_test)
 
-#model selection ({{'max_features': 17, 'min_samples_leaf': 1})
+#model selection ({'max_features': 36, 'min_samples_leaf': 1})
 params = {
     'min_samples_leaf' : [x for x in range(1, 6)],
     'max_features' : [x for x in range(5, 37)]
 }
 
-clf = GridSearchCV(RandomForestClassifier(), params, cv = 5, n_jobs = 4)
+clf = GridSearchCV(RandomForestClassifier(random_state = 42), params, cv = 5, n_jobs = 4)
 clf.fit(X_train, y_train)
 
 print(clf.best_params_)
 
 #train algorithm
-model = RandomForestClassifier(min_samples_leaf = clf.best_params_['min_samples_leaf'], max_features = clf.best_params_['max_features'])
+model = RandomForestClassifier(min_samples_leaf = clf.best_params_['min_samples_leaf'], max_features = clf.best_params_['max_features'], random_state = 42)
 model.fit(X_train, y_train)
 
-#model evaluation on the test data (score = 0.8277027027027027)
+#model evaluation on the test data (score = 0.8412162162162162)
 y_pred = model.predict(X_test)
 print(metrics.accuracy_score(y_test, y_pred))
 
-#model evaluation using cross validation (score = 0.8133065347351061)
-model = RandomForestClassifier(min_samples_leaf = clf.best_params_['min_samples_leaf'], max_features = clf.best_params_['max_features'])
+#model evaluation using cross validation (score = 0.8112760255617399)
+model = RandomForestClassifier(min_samples_leaf = clf.best_params_['min_samples_leaf'], max_features = clf.best_params_['max_features'], random_state = 42)
 
 scaler.fit(X)
 X = scaler.transform(X)
